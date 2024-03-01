68c56b0c99fb6fb0d663f774d81f2968e0c83023
Ojo Morakinyo Samuel <arayosam@gmail.com>
2024-03-01 11:41:25 +0100

    saved

--- component/Chat.jsx
+++ component/Chat.jsx
@@ -7,7 +7,7 @@ const Chat = () => {
   const [allMessages, setAllMessages] = useState([]);
 
   const [socket, setsocket] = useState("")
-  let endpoint = "http://localhost:4000"
+  let endpoint = "https://phem.vercel.app/"
 
 useEffect(() => {
     setsocket(socketClient(endpoint))   
